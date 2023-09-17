c156f78c6fed6d206aabb102c974d85ff2e2e2ba
Pozhar <abrakatabr@mail.ru>
2023-09-17 19:58:39 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] array1 = new short[10];
+        String[] array2 = new String[100500];
+        float[] array3 = new float[40];
+    }
+}
