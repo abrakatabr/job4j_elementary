0a57ec359a409ee047a93aea40402c6b0087851e
Pozhar <abrakatabr@mail.ru>
2024-01-11 14:36:38 +0300

    6.2.2. Переставить элементы массива [#226795]

---
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -0,0 +1,69 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class SwitchArrayTest {
+
+    @Test
+    void whenSwap0to3() {
+        int[] input = {1, 2, 3, 4};
+        int source = 0;
+        int destination = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, destination);
+        int[] expected = {4, 2, 3, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap0to2() {
+        int[] input = {1, 4, 5, 6, 2};
+        int source = 0;
+        int destination = 2;
+        int[] expected = {5, 4, 1, 6, 2};
+        int[] result = SwitchArray.swap(input, source, destination);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwap2to4() {
+        int[] input = {1, 3, 5, 11, 18, 0};
+        int source = 2;
+        int destination = 4;
+        int[] expected = {1, 3, 18, 11, 5, 0};
+        int[] result = SwitchArray.swap(input, source, destination);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwapBorderArrayLengthIs4() {
+        int[] input = {1, 2, 3, 4};
+        int[] result = SwitchArray.swapBorder(input);
+        int[] expected = {4, 2, 3, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwapBorderArrayLengthIs6() {
+        int[] input = {1, 2, 3, 4, 5, 6};
+        int[] result = SwitchArray.swapBorder(input);
+        int[] expected = {6, 2, 3, 4, 5, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwapBorderArrayLengthIs3() {
+        int[] input = {1, 2, 3};
+        int[] result = SwitchArray.swapBorder(input);
+        int[] expected = {3, 2, 1};
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void whenSwapBorderArrayLengthIs1() {
+        int[] input = {1};
+        int[] result = SwitchArray.swapBorder(input);
+        int[] expected = {1};
+        assertThat(result).containsExactly(expected);
+    }
+}
