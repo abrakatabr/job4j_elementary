c8548f6946e945bef5045d3e586b63ca20f82b25
Pozhar <abrakatabr@mail.ru>
2024-01-12 22:56:28 +0300

    6.5. Поиск индекса в диапазоне. [#156318

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -23,4 +23,59 @@ class FindLoopTest {
         int expected = -1;
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    public void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = new int[] {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int element = 8;
+        int start = 2;
+        int finish = 5;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = new int[] {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        int element = 4;
+        int start = 1;
+        int finish = 8;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int element = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = {1, 2, 0, 5, 4, 11, 15};
+        int element = 8;
+        int start = 0;
+        int finish = 6;
+        int expected = -1;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenDiapasonHas7Then4() {
+        int[] data = {1, 2, 2, 5, 7, 8, 9, 0};
+        int element = 7;
+        int start = 2;
+        int finish = 6;
+        int expected = 4;
+        int result = FindLoop.indexInRange(data, element, start, finish);
+        assertThat(result).isEqualTo(expected);
+    }
 }
